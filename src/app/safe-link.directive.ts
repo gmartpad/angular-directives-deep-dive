237acb12e8560fbfabebf884cefe9470f7b76927
Gabriel_Martins_Padoin <gabriel.padoin@woli.com.br>
2025-01-10 19:04:18 -0300

    [NG] INPUTS_IN_CUSTOM_DIRECTIVES - feat: implementing input inside the safe-link directive

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: '[appSafeLink]',
@@ -8,6 +8,7 @@ import { Directive } from '@angular/core';
   }
 })
 export class SafeLinkDirective {
+  queryParam = input('myapp', { alias: 'appSafeLink' })
 
   constructor() {
     console.log('SafeLinkDirective is active!')
@@ -17,6 +18,8 @@ export class SafeLinkDirective {
     const wantsToLeave = window.confirm('Do you want to leave the app?')
 
     if(wantsToLeave) {
+      const address = (event.target as HTMLAnchorElement).href;
+      (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam()
       return;
     }
 
